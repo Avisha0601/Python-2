716bdf923e2cd13c3cff1b3a27233e1846c380c5
Avisha0601 <86399682+Avisha0601@users.noreply.github.com>
2022-09-22 20:55:40 +0530

    Add files via upload

---
+++ python2.py
@@ -0,0 +1,11 @@
+String1 = "hello"
+print("Initial String: ")
+print(String1)
+
+# Printing First character
+print("/nFirst character of String is:")
+print(string1[0])
+
+# Print Last character
+print("/nLast character of String is: ")
+print(String1[-1])
